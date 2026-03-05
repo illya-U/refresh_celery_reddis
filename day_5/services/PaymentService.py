0aa0af830c448a32628e60e1f143b4d64e4546cc
Illia Ustymenko <illakust11@gmail.com>
2026-03-05 20:16:04 +0200

    five day last commit

--- day_5/services/PaymentService.py
+++ day_5/services/PaymentService.py
@@ -14,13 +14,12 @@ class PaymentService:
     def pay_order(self, payment_id):
         with transaction.atomic():
             payment = Payment.objects.select_for_update().select_related("order").get(pk=payment_id)
-            order = Order.objects.select_for_update().get(pk=payment.order.id)
-            self._pay_business_validation(payment, order)
+            self._pay_business_validation(payment, payment.order)
             payment.status = PaymentStatus.PROCESSING
             payment.save()
 
-            payment_provider_class = PAYMENT_PROVIDER_TYPE_TO_PAYMENT_PROVIDER[payment.provider]
-            operation_status = payment_provider_class().pay(payment.amount)
+        payment_provider_class = PAYMENT_PROVIDER_TYPE_TO_PAYMENT_PROVIDER[payment.provider]
+        operation_status = payment_provider_class().pay(payment.amount)
 
         with transaction.atomic():
             payment = Payment.objects.select_for_update().select_related("order").get(pk=payment_id)
@@ -32,7 +31,6 @@ class PaymentService:
             else:
                 payment.status = PaymentStatus.FAILED
 
-            print("___PAYMENT___")
             payment.save()
             order.save()
         return payment
