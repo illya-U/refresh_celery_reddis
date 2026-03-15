920b5aa3f3fa8f9350ffa6fe70a8a00756425b3e
Illia Ustymenko <illakust11@gmail.com>
2026-03-15 13:44:07 +0200

    9 day second commit

--- day_9/urls.py
+++ day_9/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from day_9.views import celery_test, celery_test_with_ack
+from day_9.views import celery_test, celery_test_with_ack, celery_task_send_to_two_queues
 
 urlpatterns = [
     path("celery_test/", celery_test),
-    path("celery_test_with_ack/", celery_test_with_ack)
+    path("celery_test_with_ack/", celery_test_with_ack),
+    path("celery_task_send_to_two_queues/", celery_task_send_to_two_queues)
 ]
--- day_9/views.py
+++ day_9/views.py
@@ -4,13 +4,14 @@ from celery import shared_task
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from refresh_celery_reddis.celery import app
+
 
 @csrf_exempt
 def celery_test(request):
     celery_task = add.delay(2, 3)
     print(celery_task.status)
     value = celery_task.get()
-    print(value)
     value = "wait"
 
     return JsonResponse(data={"value": value}, status=200)
@@ -24,14 +25,12 @@ def celery_test_with_ack(request):
     return JsonResponse(data={"value": value}, status=200)
 
 
-@shared_task(bind=True, max_retries=3)
+@app.task(rate_limit="1/m", bind=True, max_retries=3)
 def add(self, x, y):
     print("self.request", self.request)
     print("self.request.id", self.request.id)
     print("self.max_retries", self.max_retries)
     print("self.request.retries", self.request.retries)
-    if self.request.retries != 3:
-        raise self.retry(exc="wow", countdown=1)
     return self.request.retries
 
 
@@ -40,6 +39,32 @@ def add_with_ack(self, x, y):
     print("sss")
     sleep(200)
     raise 10/0
-from django.shortcuts import render
+
+
+# change on command: celery -A refresh_celery_reddis worker --loglevel=info -Q high_priority,default
+# for work
+@csrf_exempt
+def celery_task_send_to_two_queues(request):
+    priority_task = add_with_ack.apply_async(args=[2, 3], queue="high_priority")
+    default_task_0 = add_with_ack.apply_async(args=[2, 3], queue="default")
+    default_task_1 = add_with_ack.apply_async(args=[2, 3], queue="default")
+    default_task_2 = add_with_ack.apply_async(args=[2, 3], queue="default")
+    default_task_3 = add_with_ack.apply_async(args=[2, 3], queue="default")
+
+    return JsonResponse(data=
+        {
+            "priority_task.status": priority_task.status,
+            "default_task_0": default_task_0.status,
+            "default_task_1": default_task_1.status,
+            "default_task_2": default_task_2.status,
+            "default_task_3": default_task_3.status,
+        }
+    )
+
+
+
+
+
+
 
 # Create your views here.
